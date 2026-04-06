c0b1cef6301191326778fdb380803531788805ad
Beatriz-Cannaval <cannavalbeatriz@gmail.com>
2026-04-06 14:31:13 -0300

    Update Ex 07.cpp

--- Ex 07.cpp
+++ Ex 07.cpp
@@ -1,9 +1,30 @@
 #include <iostream>
-
 using namespace std;
 
-int main()
-{
-    cout << "Hello world!" << endl;
+int main() {
+    int vetor[10];
+    int *ptr;
+
+    // Entrada de dados
+    cout << "Digite 10 valores:\n";
+    for(int i = 0; i < 10; i++) {
+        cin >> vetor[i];
+    }
+
+    // Ponteiro aponta para o início do vetor
+    ptr = vetor;
+
+    // Impressão na ordem normal
+    cout << "\nOrdem normal:\n";
+    for(int i = 0; i < 10; i++) {
+        cout << *(ptr + i) << " ";
+    }
+
+    // Impressão na ordem inversa
+    cout << "\nOrdem inversa:\n";
+    for(int i = 9; i >= 0; i--) {
+        cout << *(ptr + i) << " ";
+    }
+
     return 0;
 }
